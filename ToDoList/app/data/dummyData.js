8b13ed959a2863dc54c17443e6d3d9ead0e76cd5
Abhipsha BHATTA <bhattaabhipsha@gmail.com>
2025-01-23 20:28:14 +0100

    Initial commit

---
+++ ToDoList/app/data/dummyData.js
@@ -0,0 +1,7 @@
+// Sample tasks to load initially
+export default [
+    { id: '1', title: 'Buy groceries' },
+    { id: '2', title: 'Go on walk' },
+    { id: '3', title: 'Finish React Native project' },
+  ];
+  
---
+++ ToDoList/app/styles/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 10,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 10,
+    padding: 8,
+    borderRadius: 4,
+  },
+});
